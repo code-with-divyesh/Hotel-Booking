0061dbd1ffa56218bba59dee21e4b3ee9ef842ba
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2025-08-01 23:02:32 +0530

    hero section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
-import { useLocation } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./page/Home/Home";
 
 const App = () => {
   const isOwnerpath = useLocation().pathname.includes("owner");
-  return <div>{!isOwnerpath && <NavBar />}</div>;
+  return (
+    <div>
+      {!isOwnerpath && <NavBar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;

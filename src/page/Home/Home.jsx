38dd299b9a9c094a99ea35239316a42acf21af60
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2025-08-05 17:03:51 +0530

    Testimonial component completed

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../../Components/Hero/Hero";
 import FeaturedDestination from "../../Components/FeaturedDestination/FeaturedDestination";
 import ExclusiveOffers from "../../Components/ExclusiveOffers/ExclusiveOffers";
+import Testimonials from "../../Components/Testimonials/Testimonials";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <FeaturedDestination />
       <ExclusiveOffers />
+      <Testimonials />
     </>
   );
 };
